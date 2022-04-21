2bca3776da86f9a749b8efb0899503e2e6e4f89d
GZ4003 <you@example.com>
2022-04-21 14:14:22 -0300

    upload

---
+++ src/constants/colors.js
@@ -0,0 +1,8 @@
+export default {
+    primaryColor: '#ef233c',
+    secondaryColor: '#8d99ae',
+    textColor: '#212121',
+    backgroundColor: '#edf2f4',
+    white: '#FFFFFF',
+    black: '#000000',
+}
---
+++ src/screens/place-detail/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Text } from "react-native";
+import { styles } from "./styles";
+
+const PlaceDetail = () => {
+    return (
+        <View style={styles.container}>
+            <Text>Detalles del lugar</Text>
+        </View>
+    )
+}
+
+export default PlaceDetail;
